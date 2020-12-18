c95b9717a9c666969a593b398a1c33980ce6a570
VinuBalagopalAP <vinubalagopalap@gmail.com>
2020-12-18 20:45:40 +0530

    name and id

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello World!")
+	name := "Vinu Balagopal A P"
+	id := 15
+	mark := 81.33
+	fmt.Printf("My name is %s & my id is %d, also my mark is %f", name, id, mark)
 }
